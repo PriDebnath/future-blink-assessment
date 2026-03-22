06c675134b00acb783b1e0bf2a22a38e789e41fb
Pritam <debnathpritam0802@gmail.com>
2026-03-22 17:08:02 +0530

    fixed layout

--- frontend/src/page/flow/flow.tsx
+++ frontend/src/page/flow/flow.tsx
@@ -7,6 +7,8 @@ import { useAskAi } from "@/hook/use-ask-ai";
 import { useSaveFlow } from "@/hook/use-save-flow";
 import { useGetFlows } from "@/hook/use-get-flows";
 import DeleteFlow from "@/feature/flow/component/delete-flow";
+import { Button } from "@/components/ui/button";
+import { File, PlayIcon } from "lucide-react";
 
 const nodeTypes = {
   inputNode: InputNode,
@@ -16,7 +18,7 @@ const nodeTypes = {
 function Flow() {
   const { askAi } = useAskAi();
   const { saveFlow } = useSaveFlow();
-  const { data: savedFlows, isLoading:  isLoadingSavedFlows} = useGetFlows();
+  const { data: savedFlows, isLoading: isLoadingSavedFlows } = useGetFlows();
 
   const [prompt, setPrompt] = useState("What is the capital of India?");
   const [response, setResponse] = useState("del");
@@ -49,7 +51,7 @@ function Flow() {
     let message = res?.response || res?.error;
     setResponse(message);
   };
-console.log({savedFlows});
+  console.log({ savedFlows });
 
   const handleSaveFlow = async () => {
     if (!prompt || !response) return;
@@ -58,83 +60,88 @@ console.log({savedFlows});
   };
 
   return (
-    <div className="h-screen grid grid-cols-12 gap-2 p-2">
-      
-      {/* LEFT PANEL */}
-      <div className="col-span-10 flex flex-col border-4 rounded-2xl overflow-hidden">
-        
-        {/* TOP BAR */}
-        <div className="flex gap-2 p-2 border-b bg-gray-200 ">
-          <button
-            onClick={handleRunFlow}
-            className="px-3 py-1 bg-blue-500 text-white rounded"
-          >
-            Run Flow
-          </button>
-          <button
-            onClick={handleSaveFlow}
-            className="px-3 py-1 bg-green-500 text-white rounded"
-          >
-            Save Flow
-          </button>
-        </div>
+    <div className="h-screen  flex flex-col">
+      <h1 className="p-2 bg-gray-200">FutureBlink Assessment (MERN stack)</h1>
+      <div className="flex-1 min-h-0 grid grid-cols-12 gap-2 p-2">
+        {/* LEFT PANEL */}
+        <div className="col-span-10 flex flex-col border-4 rounded-2xl overflow-hidden">
 
-        {/* FLOW AREA */}
-        <div className="flex-1">
-          <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes}>
-            <Background />
-            <Controls />
-          </ReactFlow>
-        </div>
-      </div>
+          {/* TOP BAR */}
+          <div className="flex justify-end gap-2 p-2 border-b bg-gray-200 ">
+            <Button
+              onClick={handleRunFlow}
+              // variant={''}
+              className="px-3 py-1 bg-green-500  text-white rounded-xl"
+            >
+              <PlayIcon />  Run Flow
+            </Button>
+            <Button
+              onClick={handleSaveFlow}
 
-      {/* RIGHT PANEL */}
-      <div className="col-span-2 border-4 rounded-2xl ">
-        <h2 className="font-semibold m-0 bg-gray-200 p-2">Saved Flows</h2>
-        <div className="p-2">
-          
-
-
-  {/* Loader */}
-  {isLoadingSavedFlows && (
-    <div className="flex flex-col  justify-center flex-1">
-      {/* <div className="animate-spin h-6 w-6 border-2 border-gray-400 border-t-transparent rounded-full" /> */}
-    <div className="h-4 w-3/4 bg-gray-300 rounded mb-3 animate-pulse  delay-500" />
-      <div className="h-4 w-full bg-gray-300 rounded mb-2 animate-pulse delay-1000" />
-      <div className="h-4 w-5/6 bg-gray-300 rounded mb-4 animate-pulse delay-200" />
-    </div>
-  )}
-
-  {/* Empty state */}
-  {!isLoadingSavedFlows && savedFlows?.length === 0 && (
-    <div className="text-sm text-gray-500 text-center mt-4">
-      No flows saved
-    </div>
-  )}
-
-  {/* List */}
-  {!isLoadingSavedFlows && savedFlows &&  savedFlows?.length! > 0 && (
-    <div className="flex flex-col gap-2 overflow-y-auto">
-      {savedFlows.map((flow) => (
-        <div
-          key={flow._id}
-          className="p-2 border rounded hover:bg-gray-50 cursor-pointer transition flex items-center justify-between"
-        >
-          <div className="text-sm font-medium truncate">
-            {flow.prompt}
+              className="px-3 py-1 bg-blue-500  text-white rounded"
+            >
+              <File />
+              Save Flow
+            </Button>
           </div>
-          <DeleteFlow data={flow}/>
-         {/*   <div className="text-xs text-gray-500 truncate">
+
+          {/* FLOW AREA */}
+          <div className="flex-1">
+            <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes}>
+              <Background />
+              <Controls />
+            </ReactFlow>
+          </div>
+        </div>
+
+        {/* RIGHT PANEL */}
+        <div className=" col-span-2 border-4 rounded-2xl  flex flex-col min-h-0">
+          <h2 className="font-semibold m-0 bg-gray-200 p-2">Saved Flows</h2>
+          <div className="p-2  flex-1 min-h-0 flex flex-col">
+
+
+
+            {/* Loader */}
+            {isLoadingSavedFlows && (
+              <div className="flex flex-col  justify-center flex-1">
+                {/* <div className="animate-spin h-6 w-6 border-2 border-gray-400 border-t-transparent rounded-full" /> */}
+                <div className="h-4 w-3/4 bg-gray-300 rounded mb-3 animate-pulse  delay-500" />
+                <div className="h-4 w-full bg-gray-300 rounded mb-2 animate-pulse delay-1000" />
+                <div className="h-4 w-5/6 bg-gray-300 rounded mb-4 animate-pulse delay-200" />
+              </div>
+            )}
+
+            {/* Empty state */}
+            {!isLoadingSavedFlows && savedFlows?.length === 0 && (
+              <div className="text-sm text-gray-500 text-center mt-4">
+                No flows saved
+              </div>
+            )}
+
+            {/* List */}
+            {!isLoadingSavedFlows && savedFlows && savedFlows?.length! > 0 && (
+              <div className="flex-1 flex flex-col gap-2 overflow-y-auto">
+                {savedFlows.map((flow) => (
+                  <div
+                    key={flow._id}
+                    className="p-2 border rounded hover:bg-gray-50 cursor-pointer transition flex items-center justify-between"
+                  >
+                    <div className="text-sm font-medium truncate">
+                      {flow.prompt}
+                    </div>
+                    <DeleteFlow data={flow} />
+                    {/*   <div className="text-xs text-gray-500 truncate">
            {flow.response}
           </div> */}
-        </div>
-      ))}
-    </div>
-  )}
+                  </div>
+                ))}
+              </div>
+            )}
 
+          </div>
         </div>
+
       </div>
-
     </div>
   );
 }
